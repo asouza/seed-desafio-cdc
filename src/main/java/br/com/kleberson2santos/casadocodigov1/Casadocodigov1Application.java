bb7fc09d5a837af383360e62e2fdbd9c44570f09
Kleberson Santos <kleberson2santos@gmail.com>
2021-03-21 14:00:42 -0400

    configuração inicial

---
+++ src/main/java/br/com/kleberson2santos/casadocodigov1/Casadocodigov1Application.java
@@ -0,0 +1,13 @@
+package br.com.kleberson2santos.casadocodigov1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Casadocodigov1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Casadocodigov1Application.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/kleberson2santos/casadocodigov1/Casadocodigov1ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.kleberson2santos.casadocodigov1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Casadocodigov1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
