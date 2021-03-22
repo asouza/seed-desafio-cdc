fa1406b3d0d2a45ea9d5a282c2eadcb95b6ebe32
Armando Tadeu Del Col <atdc.codemaster@gmail.com>
2021-03-22 16:27:31 -0300

    Trocando a Annotation do EntityManager

--- src/main/java/br/com/armando/decasadocodigo/api/controller/AuthorController.java
+++ src/main/java/br/com/armando/decasadocodigo/api/controller/AuthorController.java
@@ -2,19 +2,19 @@ package br.com.armando.decasadocodigo.api.controller;
 
 import br.com.armando.decasadocodigo.api.model.input.AuthorInput;
 import br.com.armando.decasadocodigo.domain.model.Author;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
 import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/authors")
 public class AuthorController {
 
-    @Autowired
+    @PersistenceContext
     private EntityManager manager;
 
     @PostMapping
