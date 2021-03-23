a4404ccefc6dd001315ea205a3075fa6875df57d
Armando Tadeu Del Col <atdc.codemaster@gmail.com>
2021-03-22 22:15:31 -0300

    removendo os getters and setters do AuthorRequest pois estavam desnecessários no momento

--- src/main/java/br/com/armando/decasadocodigo/api/model/request/AuthorRequest.java
+++ src/main/java/br/com/armando/decasadocodigo/api/model/request/AuthorRequest.java
@@ -25,35 +25,11 @@ public class AuthorRequest {
         this.description = description;
     }
 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
     public Author toModel() {
         Author author = new Author(
-                this.getName(),
-                this.getEmail(),
-                this.getDescription()
+                this.name,
+                this.email,
+                this.description
         );
         return author;
     }
