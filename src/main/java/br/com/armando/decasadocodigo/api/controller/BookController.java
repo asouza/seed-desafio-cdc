7b5e9466a9e3935fd2cc784edf439634ee02d2af
Armando Tadeu Del Col <atdc.codemaster@gmail.com>
2021-03-29 20:39:56 -0300

    Adicionando busca de Livro por Id

--- src/main/java/br/com/armando/decasadocodigo/api/controller/BookController.java
+++ src/main/java/br/com/armando/decasadocodigo/api/controller/BookController.java
@@ -5,6 +5,7 @@ import br.com.armando.decasadocodigo.api.model.response.BookResponse;
 import br.com.armando.decasadocodigo.api.model.response.BookSummaryResponse;
 import br.com.armando.decasadocodigo.domain.model.Book;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
@@ -36,6 +37,16 @@ public class BookController {
         return manager.createQuery("SELECT book FROM Book book join fetch book.author join fetch book.category", Book.class)
                 .getResultList().stream().map(book -> new BookSummaryResponse(book))
                 .collect(Collectors.toList());
+    }
+
+    @GetMapping(value = "/{bookId}")
+    public ResponseEntity<BookResponse> findById(@PathVariable Long bookId) {
+        Book book = manager.find(Book.class, bookId);
+        if (book == null) {
+            return ResponseEntity.notFound().build();
+        } else {
+            return ResponseEntity.ok(new BookResponse(book));
+        }
     }
 
 }
