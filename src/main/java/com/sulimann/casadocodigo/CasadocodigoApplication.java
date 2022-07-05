618f5a8423d62ffa12a12d52640b346adedf1679
Rafael Sulimann <sulimannrafael@gmail.com>
2022-07-04 21:53:12 -0300

    Criação do projeto da Casa do Codigo com Spring Boot

---
+++ src/main/java/com/sulimann/casadocodigo/CasadocodigoApplication.java
@@ -0,0 +1,13 @@
+package com.sulimann.casadocodigo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CasadocodigoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CasadocodigoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sulimann/casadocodigo/CasadocodigoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sulimann.casadocodigo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CasadocodigoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
