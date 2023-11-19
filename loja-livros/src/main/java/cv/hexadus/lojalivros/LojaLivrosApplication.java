92d74c6a580055da1b0468078355df6ff3b51047
bortet <bruno.ortet@gmail.com>
2023-11-19 21:36:06 -0100

    spring boot project added

---
+++ loja-livros/src/main/java/cv/hexadus/lojalivros/LojaLivrosApplication.java
@@ -0,0 +1,13 @@
+package cv.hexadus.lojalivros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LojaLivrosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LojaLivrosApplication.class, args);
+	}
+
+}
