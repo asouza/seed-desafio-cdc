48a60b8a87c9f7357ee05b74a5bc577014883d51
Rui Nascimento <sruy19@gmail.com>
2024-01-15 10:59:26 -0300

    feat: entry point of application

---
+++ src/main/java/br/com/rsfot/bookstore/RBookstoreApplication.java
@@ -0,0 +1,13 @@
+package br.com.rsfot.bookstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RBookstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RBookstoreApplication.class, args);
+	}
+
+}
